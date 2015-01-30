dc804a0cf44007a8a4512124419af40396622a03
Alexander Kamyanskiy <alexander.kamyanskiy@willowcode.com>
2015-01-30 03:45:05 +0300

    add base bootstrap theme, add 3 buttons for auth by social networks and email auth

--- cookbook/views.py
+++ cookbook/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render_to_response, redirect, render
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 # from django.template.context import RequestContext
+from cookbook.decorators import render_to
 
+def context(**extra):
+    return dict(**extra)
 
 def login(request):
     # context = RequestContext(request, {
@@ -17,5 +20,25 @@ def home(request):
 
 
 def logout(request):
+    """Logs out user"""
     auth_logout(request)
     return redirect('/')
+
+@login_required
+@render_to('home.html')
+def done(request):
+    """Login complete view, displays user data"""
+    return context()
+
+
+@render_to('login.html')
+def validation_sent(request):
+    return context(
+        validation_sent=True,
+        email=request.session.get('email_validation_address')
+    )
+
+@render_to('login.html')
+def require_email(request):
+    backend = request.session['partial_pipeline']['backend']
+    return context(email_required=True, backend=backend)
