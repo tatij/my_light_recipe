ac29e285f77c49a3865cf63014db0bfb8431e5c1
tatij <kolosok.tatiana@gmail.com>
2015-02-02 13:09:17 +0300

    new edit #5

--- receipes/models.py
+++ receipes/models.py
@@ -1,38 +1,31 @@
 from django.db import models
 
 class User(models.Model):
-    id_user = models.ForeignKey('Book')
-    name_user = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
-    id_book = models.ForeignKey('ConnectDishBook')
-    id_user = models.ForeignKey('User')
-    name_book = models.CharField(max_length=100)
+    user = models.ForeignKey('User')
+    name = models.CharField(max_length=100)
         
 class Dish(models.Model):
-    id_dish_connect = models.ForeignKey('ConnectDishBook')
-    id_dish_recipe = models.ForeignKey('Recipe')
-    name_dish = models.CharField(max_length=100)
-    id_book = models.ManyToManyField('Book')
-    type_dish = models.ForeignKey('DishType')
+    book = models.ManyToManyField('Book')
+    name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
+    type = models.ForeignKey('DishType')
     
-class DishType(models.Model):
-    id_type = models.OneToOneField('Dish')
+class DishType(models.Model):    
     name_type = models.CharField(max_length=100)
     
 class Recipe(models.Model):
-    id_recipe = models.ForeignKey('RecipePart')
-    id_ingredient = models.ForeignKey('Ingredient')
-    id_dish_recipe = models.ForeignKey('Dish')
+    ingredient = models.OneToOneField('Ingredient')
+    dish = models.OneToOneField('Dish')
     
 class RecipePart(models.Model):
-    id_recipe = models.ForeignKey('Recipe')
-    id_ingredient = models.ForeignKey('Ingredient')
+    recipe = models.ForeignKey('Recipe')
+    ingredient = models.ForeignKey('Ingredient')
     unit = models.CharField(max_length=100)
     note = models.CharField(max_length=100)
     
 class Ingredient(models.Model):
-    id_ingredient = models.ForeignKey('RecipePart')
     name_ingredient = models.CharField(max_length=100)
     note = models.CharField(max_length=1000)
