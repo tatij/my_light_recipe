c461bed3049e346a99c16bc386cdd449bf90ccb7
tatij <kolosok.tatiana@gmail.com>
2015-02-02 10:45:34 +0300

    str 18, 19, 34 #5

--- receipes/models.py
+++ receipes/models.py
@@ -12,11 +12,11 @@ class Book(models.Model):
 class ConnectDishBook(models.Model):
     id = models.IntegerField(default=0)
     id_book = models.ForeignKey('Book')
-    id_dish = models.ForeignKey('Book')
+    id_dish_connect = models.ForeignKey('Dish')
     
 class Dish(models.Model):
-    id_dish = models.ForeignKey('ConnectDishBook')
-    id_dish = models.ForeignKey('Recipe')
+    id_dish_connect = models.ForeignKey('ConnectDishBook')
+    id_dish_recipe = models.ForeignKey('Recipe')
     name_dish = models.CharField(max_length=100)
     id_book = models.IntegerField(default=0)
     type_dish = models.ForeignKey('DishType')
@@ -29,17 +29,15 @@ class DishType(models.Model):
 class Recipe(models.Model):
     id_recipe = models.ForeignKey('RecipePart')
     id_ingredient = models.ForeignKey('Ingredient')
-    id_dish = models.ForeignKey('Dish')
+    id_dish_recipe = models.ForeignKey('Dish')
     
 class RecipePart(models.Model):
-    id = models.IntegerField(default=0)
     id_recipe = models.ForeignKey('Recipe')
     id_ingredient = models.ForeignKey('Ingredient')
     unit = models.CharField(max_length=100)
     note = models.CharField(max_length=100)
     
 class Ingredient(models.Model):
-    id_ingredient = models.ForeignKey('Recipe')
-    id_ingredient = models.ForeignKey('Recipe')
+    id_ingredient = models.ForeignKey('RecipePart')
     name_ingredient = models.CharField(max_length=100)
     note = models.CharField(max_length=1000)
